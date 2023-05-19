756879390645dd5db7cd988d016cce0ff4d0a1e8
Farruh Kushnazarov <33511681+k-farruh@users.noreply.github.com>
2023-05-20 02:35:18 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -19,10 +19,10 @@ transform_test = transforms.Compose([
 ])
 
 # Load CIFAR100 dataset
-trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=False, transform=transform_train)
+trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=False, transform=transform_test)
+testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 
 # Define ResNet50 model
